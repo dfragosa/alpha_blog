00af02e735491fc1f18a453c4d4c59dbfd431fa0
dfragosa <fderric@gmail.com>
2021-10-12 12:59:30 -0400

    added Edit/Update/Validation features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,35 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all 
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		if @article.save
+			flash[:notice] = "Article was created successfuly."
+		redirect_to article_path(@article) #shortcut is redirect_to @article
+		else
+			render 'new'
+	 	end
+	end
+
+	def edit
+
+		@article = Article.find(params[:id])
+
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(params.require(:article).permit(:title, :description))
+			flash[:notice] = "Article was save successfully"
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	
 end 
