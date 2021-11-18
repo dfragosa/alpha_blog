b314917a0057342806d68afadde78fc89b34bb13
dfragosa <fderric@gmail.com>
2021-11-17 20:36:06 -0500

    Add new users signup form

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
  # or exposing all the RESTful routes using ex below
  resources :articles
+ get 'signup', to: 'users#new'
+ resources :users, except: [:new]
+
    end
