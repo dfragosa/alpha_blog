456dd649cb1fdb118e5c59afe3b35e9311f3d797
dfragosa <fderric@gmail.com>
2021-11-28 21:01:31 -0500

    Add controller methods as helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,8 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 		#hardcoded 4 now 4 developing purposes
-		@article.user = User.first 
+		#@article.user = User.first 
+		@article.user = current_user
 		if @article.save
 			flash[:notice] = "Article was created successfuly."
 		redirect_to article_path(@article) #shortcut is redirect_to @article
